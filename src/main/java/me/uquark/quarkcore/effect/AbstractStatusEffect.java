2efca04f288f831f4085e12758ddcd2305dd50f0
Artemii Bigdan <artem@bigdan.in>
2021-12-08 15:47:22 +0200

    Update to 1.18

--- src/main/java/me/uquark/quarkcore/effect/AbstractStatusEffect.java
+++ src/main/java/me/uquark/quarkcore/effect/AbstractStatusEffect.java
@@ -1,14 +1,14 @@
 package me.uquark.quarkcore.effect;
 
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectType;
+import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public abstract class AbstractStatusEffect extends StatusEffect {
     public final Identifier id;
 
-    protected AbstractStatusEffect(String modid, String name, StatusEffectType type, int color) {
+    protected AbstractStatusEffect(String modid, String name, StatusEffectCategory type, int color) {
         super(type, color);
         id = new Identifier(modid, name);
     }
